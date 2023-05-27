27209eb1b60be2f4da82faa8d736c3ab6d2e6f33
Sicherheitsschmiede <ui.manufaktur@gmail.com>
2023-05-27 08:41:16 +0200

    Rename

---
+++ source/web/ui5/css.d
@@ -0,0 +1,22 @@
+﻿module web.ui5.css;
+
+import web.ui5;
+
+class DUI5Css : DUI5AppObj {
+	this() { super(); }
+	this(DUI5App myApp) { super(myApp); }
+	this(string someContent) { super(someContent); }
+	this(DUI5App myApp, string someContent) { super(myApp, someContent); }
+
+	void request(HTTPServerRequest req, HTTPServerResponse res) {
+		res.writeBody(toString, "text/css");
+	}
+	override string toString() {
+		return (_content) ? _content : "";
+	}
+}
+auto UI5Css() { return new DUI5Css; }
+
+unittest {
+	auto css = UI5Css;
+}
